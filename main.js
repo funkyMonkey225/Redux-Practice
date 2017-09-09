7057b6150b9defb3e81ae8dd3a978ecd2008e6ae
Mac User <sabbey37@gmail.com>
2017-09-09 11:51:22 -0400

    Update async and odd functions

--- main.js
+++ main.js
@@ -13,3 +13,35 @@ const counter = (state, action) => {
             return state;
     }
 };
+
+const store = Redux.createStore(counter);
+const valueEl = document.querySelector('#value');
+const inc = document.querySelector('#increment');
+const dec = document.querySelector('#decrement');
+const incIfOdd = document.querySelector('#incrementIfOdd');
+const incAsync = document.querySelector('#incrementAsync');
+
+const render = () => {
+    // console.log(store.getState());
+    valueEl.innerHTML = store.getState().toString();
+}
+
+render();
+store.subscribe(render);
+
+inc.addEventListener('click', () => store.dispatch({type: 'INCREMENT'}));
+dec.addEventListener('click', () => store.dispatch({type: 'DECREMENT'}));
+
+incIfOdd.addEventListener('click', () => {
+  if (store.getState() % 2 !== 0) {
+    store.dispatch({type: 'INCREMENT'});  
+  }
+});
+
+incAsync.addEventListener('click', () => {
+  setTimeout(() => {
+    store.dispatch({type: 'INCREMENT'});
+  }, 2000);
+});
+
+// store.dispatch({type: 'INCREMENT'})
