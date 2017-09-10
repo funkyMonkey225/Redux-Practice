f2f00a412b140c94769741a854bb1846fb877d40
Mac User <sabbey37@gmail.com>
2017-09-10 19:27:48 -0400

    Adds styling to videolistitems

--- Redux-Simple-Starter/src/components/videolist.js
+++ Redux-Simple-Starter/src/components/videolist.js
@@ -2,10 +2,12 @@ import React, {Component} from 'react';
 import VideoListItem from './videolistitem';
 
 const VideoList = (props) => {
-    
+    const videoItems = props.videos.map((video) => {
+        return <VideoListItem key={video.etag} video={video} />
+    })
     return (
         <ul className="col-md-4 list-group">
-    
+            {videoItems}
         </ul>
     );
 }
