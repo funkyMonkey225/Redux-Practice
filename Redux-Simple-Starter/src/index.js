c8de980e6a4ab90a5d6a20cb2d28a329c8566924
Mac User <sabbey37@gmail.com>
2017-09-10 19:50:40 -0400

    Adds embed to video detail

--- Redux-Simple-Starter/src/index.js
+++ Redux-Simple-Starter/src/index.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/searchbar';
 import VideoList from './components/videolist';
+import VideoDetail from './components/videodetail';
 import { createStore, applyMiddleware } from 'redux';
 import YTSearch from 'youtube-api-search';
 
@@ -11,7 +12,10 @@ const API_KEY = 'AIzaSyD9L4feFPhk4ZIjzjm2b5apTdVO9GmYDQA';
 class App extends Component {
     constructor(props) {
         super(props);
-        this.state = {videos: []};
+        this.state = {
+            videos: [],
+            selectedVideo: null
+        };
         YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
             this.setState({videos});
         });
@@ -20,6 +24,7 @@ class App extends Component {
         return ( 
             <div>
                 <SearchBar />
+                <VideoDetail video={this.state.videos[0]}/>
                 <VideoList videos={this.state.videos} />
             </div>
         );
