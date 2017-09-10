00331ca6bc45ee70fbeda43018f4d2067ab6a87b
Mac User <sabbey37@gmail.com>
2017-09-10 19:14:34 -0400

    Adds funcitonal component to videolistitem

--- Redux-Simple-Starter/src/components/videolist.js
+++ Redux-Simple-Starter/src/components/videolist.js
@@ -1,9 +1,11 @@
 import React, {Component} from 'react';
+import VideoListItem from './videolistitem';
 
 const VideoList = (props) => {
+    
     return (
         <ul className="col-md-4 list-group">
-            {props.videos.length}
+    
         </ul>
     );
 }
