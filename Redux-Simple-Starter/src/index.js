22d3342f8d1f06f0bc56361af9fd7cbcdf58b1f1
Mac User <sabbey37@gmail.com>
2017-09-10 20:28:17 -0400

    Allows user to search for videos

--- Redux-Simple-Starter/src/index.js
+++ Redux-Simple-Starter/src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/searchbar';
@@ -16,7 +17,14 @@ class App extends Component {
             videos: [],
             selectedVideo: null
         };
-        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+    }
+
+    componentWillMount() {
+        this.videoSearch('surfboards');
+    }
+
+    videoSearch(term) {
+         YTSearch({key: API_KEY, term: term}, (videos) => {
             this.setState({
                 videos: videos,
                 selectedVideo: videos[0]
@@ -24,9 +32,10 @@ class App extends Component {
         });
     }
     render() {
+        const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300);
         return ( 
             <div>
-                <SearchBar />
+                <SearchBar onSearchTermChange={videoSearch}/>
                 <VideoDetail video={this.state.selectedVideo}/>
                 <VideoList 
                     onVideoSelect={selectedVideo => this.setState({selectedVideo})}
