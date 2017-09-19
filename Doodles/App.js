2d27d72cd16bbf6003d4b3b5f88a29f65da77882
Mac User <sabbey37@gmail.com>
2017-09-19 14:32:43 -0400

    Adds cheese image

--- Doodles/App.js
+++ Doodles/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
   render() {
+    let pic = {
+        uri: 'http://www4.pictures.zimbio.com/mp/26-j9dYPwm_l.jpg'
+    };
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text>Cheese!</Text>
+        <Image source={{uri: 'http://www4.pictures.zimbio.com/mp/26-j9dYPwm_l.jpg'}} 
+        style={{width: 193, height: 110}} />
       </View>
     );
   }
