88f94ffc516d4e3bec1f5e7350d642626e46c429
Mac User <sabbey37@gmail.com>
2017-09-19 11:49:53 -0400

    Adds counter_inferno

---
+++ counter_inferno/src/reducers.js
@@ -0,0 +1,47 @@
+import * as actions from './actions';
+import {combineReducers} from 'redux';
+import undoable, {distinctState} from 'redux-undo';
+
+const counter = (state={}, action) => {
+    switch(action.type) {
+        case actions.INCREMENT:
+            let newState = {...state};
+            newState[action.id].count += 1;
+            return newState;
+
+        case actions.DECREMENT:
+            newState = {...state};
+            newState[action.id].count -= 1;
+            return newState;
+
+        case actions.ADD_COUNTER:
+            newState = {...state};
+            newState[action.id] = {id: action.id, count: action.count }
+            return newState;
+        
+        case actions.REMOVE_COUNTER:
+            newState = {...state};
+            delete newState[action.id];
+            return newState;
+
+        default:
+            return state;
+    }
+}
+
+const marta = (state=[], action) => {
+    switch(action.type) {
+        case actions.FETCH_MARTA:
+            return action.payload.data;
+
+        default:
+            return state;
+    }
+}
+
+const rootReducer = combineReducers({
+    counter,
+    marta
+})
+
+export default rootReducer;
