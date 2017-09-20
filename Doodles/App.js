c235cd4fc0cacb32465926e393194ba32d8cdb79
Mac User <sabbey37@gmail.com>
2017-09-20 10:40:41 -0400

    Adds style to app

--- Doodles/App.js
+++ Doodles/App.js
@@ -2,25 +2,36 @@ import React from 'react';
 import { Image, StyleSheet, Text, View } from 'react-native';
 import MartaDash from './MartaDash';
 
+const styles = StyleSheet.create({
+    // No Dashes
+    // No periods
+    catBox: {
+        //style values in quotes
+        borderColor: 'purple',
+        borderWidth: 3,
+        borderStyle: 'solid'
+    },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 40
+    },
+});
+
 export default class App extends React.Component {
   render() {
     
     return (
       <View style={styles.container}>
-        <Text>Cheese!</Text>
-        <Image source={{uri: 'https://placekitten.com/193/193'}} 
-        style={{width: 193, height: 193}} />
+        <View style={styles.catBox}>
+            <Text>Cheese!</Text>
+            <Image source={{uri: 'https://placekitten.com/193/193'}} 
+            style={{width: 193, height: 193}} />
+        </View>
         <MartaDash/>
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
